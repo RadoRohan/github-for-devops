8ff5dbf5a5b79a2821fdd82f1d18c88d72e28165
rohanrado <rohan@rado.com>
2026-03-14 17:09:55 +0000

    New changes

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
+
 print("Hello friendsls")
 
-  183  ls
-  184  chmod 400 "linux-for-devops-key2.pem"
-  185  ssh
-  186  ssh -i linux-for-devops-key2.pem ubuntu@ec2-13-61-179-145.eu-north-1.compute.amazonaws.com
-  187  history
+
+
+print("Hello friends")
+
